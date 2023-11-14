9035ef7f21c7c85761e92ac617039260d6edb596
prince ajuzie <princeajuzie1@gmail.com>
2023-11-14 07:53:47 +0100

    add: new deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const   withMT = require('@material-tailwind/react/utils/withMT');
-module.exports = withMT({
+module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -21,4 +21,4 @@ module.exports = withMT({
   plugins: [
     require('rippleui')
   ],
-})
+}
